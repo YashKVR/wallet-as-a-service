21cdeb358f1a1faf6edfb0fd3e7ca8367ea1bdf8
yashkvr <yashincrypto5@gmail.com>
2024-08-04 11:51:07 +0530

    added prisma db, models and wallet creation on signup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+import db from "@/app/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { Keypair } from "@solana/web3.js";
 
 //Signup/signin with google
 const handler = NextAuth({
@@ -8,7 +10,52 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account?.provider === "google") {
+                const email = user.email;
+                if (!email) {
+                    return false;
+                }
+
+                const userDb = await db.user.findFirst({
+                    where: {
+                        username: email
+                    }
+                })
+
+                if (userDb) {
+                    return true;
+                }
+                const keypair = Keypair.generate();
+                const publicKey = keypair.publicKey.toBase58();
+                const privateKey = keypair.secretKey;
+
+                await db.user.create({
+                    data: {
+                        username: email,
+                        provider: "Google",
+                        solWallet: {
+                            create: {
+                                publicKey: publicKey,
+                                privateKey: privateKey.toString()
+                            }
+                        },
+                        inrWallet: {
+                            create: {
+                                balance: 0
+                            }
+                        }
+                    }
+                })
+
+                return true;
+            }
+
+            return false
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
